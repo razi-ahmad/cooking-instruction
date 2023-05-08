0eb63c0a97b3d2cfb5032a8e71ca28f90946e41a
razi.ahmad <razi.ahmad@justeattakeaway.com>
2023-05-08 20:29:18 +0200

    Format the files

--- src/main/java/nl/abnamro/recipe/config/SpringFoxConfig.java
+++ src/main/java/nl/abnamro/recipe/config/SpringFoxConfig.java
@@ -12,9 +12,9 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();                                           
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
